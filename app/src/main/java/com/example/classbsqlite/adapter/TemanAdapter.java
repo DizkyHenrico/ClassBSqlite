feb98e22d1e30c405947c5787f1b023ce6ef666c
DizkyHenrico <henricozdzky@gmail.com>
2022-04-14 05:06:45 +0700

    ClassBSqlite

--- app/src/main/java/com/example/classbsqlite/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/classbsqlite/adapter/TemanAdapter.java
@@ -1,64 +1,107 @@
 package com.example.classbsqlite.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.classbsqlite.MainActivity;
 import com.example.classbsqlite.R;
+import com.example.classbsqlite.database.DBcontroller;
 import com.example.classbsqlite.database.Teman;
+import com.example.classbsqlite.edit_teman;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
-    private ArrayList<Teman> ListData;
+    private ArrayList<Teman> listData;
+    private Context control;
 
     public TemanAdapter(ArrayList<Teman> listData) {
-        this.ListData = listData;
+        this.listData = listData;
     }
 
+
     @Override
-    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TemanViewHolder  onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutinf = LayoutInflater.from(parent.getContext());
-        View view = layoutinf.inflate(R.layout.row_data_teman,parent,false);
+        View view = layoutinf.inflate(R.layout.row_data_teman, parent, false);
+        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
-        String nm,tlp;
-
-        nm = ListData.get(position).getNama();
-        tlp = ListData.get(position).getTelpon();
+    public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
+        //deklarasi variabel
+        String nm,tlp, id;
+        id = listData.get(position).getId();
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+        DBcontroller db = new DBcontroller(control);
 
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setTextSize(20);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
 
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(control,holder.cardku);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.mnEdit:
+                                Intent i = new Intent(control, edit_teman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telpon", tlp);
+                                control.startActivity(i);
+                                break;
+                            case R.id.mnHapus:
+                                HashMap<String, String> values = new HashMap<>();
+                                values.put("id", id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return false;
+
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
-
-        return (ListData != null)?ListData.size() : 0;
+        return (listData != null)?listData.size() : 0;
     }
 
     public class TemanViewHolder extends RecyclerView.ViewHolder {
-
         private CardView cardku;
-        private TextView namaTxt,telponTxt;
+        private TextView namaTxt, telponTxt;
         public TemanViewHolder(View view) {
             super(view);
-            cardku = (CardView) view.findViewById(R.id.recyclerView);
+            cardku = (CardView) view.findViewById(R.id.card);
             namaTxt = (TextView) view.findViewById(R.id.textNama);
             telponTxt = (TextView) view.findViewById(R.id.textTelpon);
         }
     }
-
 }
--- app/src/main/java/com/example/classbsqlite/database/DBcontroller.java
+++ app/src/main/java/com/example/classbsqlite/database/DBcontroller.java
@@ -6,19 +6,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DBcontroller extends SQLiteOpenHelper {
-    public DBcontroller( Context context) {
+
+    public DBcontroller(Context context) {
         super(context, "ProdiTI", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table teman (id integer primary key,nama text,telpon text)");
+        db.execSQL("CREATE TABLE teman (id integer primary key, nama text,telpon text)");
 
     }
 
@@ -28,31 +27,48 @@ public class DBcontroller extends SQLiteOpenHelper {
         onCreate(db);
 
     }
-    public void InsertData(HashMap<String,String> queryValues) {
-        SQLiteDatabase basisdata = this.getReadableDatabase();
+
+    public void InsertData(HashMap<String, String> queryValues) {
+        SQLiteDatabase basisdata = this.getWritableDatabase();
         ContentValues nilai = new ContentValues();
-        nilai.put("nama",queryValues.get("nama"));
-        nilai.put("telpon",queryValues.get("telpon"));
-        basisdata.insert("teman",null,nilai);
+        nilai.put("nama", queryValues.get("nama"));
+        nilai.put("telpon", queryValues.get("telpon"));
+        basisdata.insert("teman", null, nilai);
         basisdata.close();
     }
 
-    public ArrayList<HashMap<String,String>> getAllTeman(){
-        ArrayList<HashMap<String,String >> daftarTeman;
+    public void UpdateData(HashMap<String, String> queryValues){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues nilai = new ContentValues();
+        nilai.put("nama", queryValues.get("nama"));
+        nilai.put("telpon", queryValues.get("telpon"));
+        db.update("teman", nilai, "id=?", new String[]{queryValues.get("id")});
+        db.close();
+    }
+
+    public void DeleteData(HashMap<String, String> queryValue){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete("teman", "id=?", new String[]{queryValue.get("id")});
+        db.close();
+    }
+    public ArrayList<HashMap<String,String>>getAllTeman(){
+        ArrayList<HashMap<String,String>> daftarTeman;
         daftarTeman = new ArrayList<HashMap<String,String>>();
-        String selecQuery = "Select * from teman";
+        String selectQuery = "Select * from teman";
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selecQuery,null);
+        Cursor cursor = db.rawQuery(selectQuery, null);
         if(cursor.moveToFirst()){
             do {
                 HashMap<String,String> map = new HashMap<>();
                 map.put("id",cursor.getString(0));
-                map.put("nama",cursor.getString(1));
+                map.put("nama", cursor.getString(1));
                 map.put("telpon",cursor.getString(2));
                 daftarTeman.add(map);
             }while (cursor.moveToNext());
         }
+
         db.close();
         return daftarTeman;
     }
 }
+
